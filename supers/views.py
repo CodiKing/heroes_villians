bb6c913a9cda1f5376a829caca60090a1d6bcf03
codi kingsbury <codikingsbury117@gmail.com>
2022-03-21 12:44:19 -0700

    recording last save

--- supers/views.py
+++ supers/views.py
@@ -10,11 +10,12 @@ from .models import Supers
 @api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
-        super_type = request.query_params.get('type')
+        super_type = request.query_params.get('super_type_id')
+        print(super_type)
         queryset = Supers.objects.all()
         if super_type:
-            queryset = queryset.filter(type__name = super_type)
-            serializer = SupersSerializer(queryset, many=True)
+            queryset = queryset.filter(super_type_id__name = super_type)
+        serializer = SupersSerializer(queryset, many=True)
         return Response(serializer.data)
     elif request.method == "POST":
         serializer = SupersSerializer(data=request.data)
